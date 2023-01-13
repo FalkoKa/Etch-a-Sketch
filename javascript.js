6cc9690c27d5a37a152aa25011df4e7a54858244
falko <115454465+FalkoKa@users.noreply.github.com>
2023-01-13 18:39:22 +0700

    Modify buttons and change color function

--- javascript.js
+++ javascript.js
@@ -1,10 +1,19 @@
 const grid = document.querySelector('.grid');
 const btnSize = document.querySelector('#grid-size');
+const btnColor = document.querySelector('#color');
+const btnRainbow = document.querySelector('#rainbow');
 const btnClear = document.querySelector('#clear');
+const btnErase = document.querySelector('#eraser');
+
 let gridSize = 16;
+let currentMode = 'color';
 
 btnSize.addEventListener('click', getGridSize);
 btnClear.addEventListener('click', clearGrid);
+btnErase.addEventListener('click', () => setCurrentMode('eraser'));
+btnColor.addEventListener('click', () => setCurrentMode('color'));
+btnRainbow.addEventListener('click', () => setCurrentMode('rainbow'));
+
 
 
 createSquares(gridSize);
@@ -13,17 +22,25 @@ function createSquares(size) {
     for (let i = size * size; i > 0; i--) {
         const height = 400 / size;
         const width = 400 / size;
-        const square = document.createElement('div');
-        square.style.cssText=`height: ${height}px; width: ${width}px;`;
-        square.classList.add('square');
-        square.addEventListener('mouseover', changeColor);
-        // square.addEventListener('mousedown', changeColor);
-        grid.append(square);
+        const squareDiv = document.createElement('div');
+        squareDiv.style.cssText=`height: ${height}px; width: ${width}px;`;
+        squareDiv.classList.add('square');
+        squareDiv.addEventListener('mouseover', changeColor);
+        grid.append(squareDiv);
     }
 }
 
-function changeColor () {
-    this.classList.add('hov')
+function changeColor(e) {
+    if (currentMode === 'color') {
+        e.target.style.backgroundColor = 'black';
+    } else if (currentMode === 'eraser') {
+        e.target.style.backgroundColor = 'white';
+    } else if (currentMode === 'rainbow') {
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    }
 }
 
 function getGridSize() {
@@ -45,3 +62,26 @@ function clearGridNoNewGrid() {
     const squares = document.querySelectorAll('.square');
     squares.forEach(square => square.remove());
 }
+
+function activeButton(newMode) {
+    if (currentMode === 'rainbow') {
+        btnRainbow.classList.remove('active')
+        } else if (currentMode === 'color') {
+        btnColor.classList.remove('active')
+        } else if (currentMode === 'eraser') {
+        btnErase.classList.remove('active')
+    }
+    
+    if (newMode === 'rainbow') {
+        btnRainbow.classList.add('active')
+      } else if (newMode === 'color') {
+        btnColor.classList.add('active')
+      } else if (newMode === 'eraser') {
+        btnErase.classList.add('active')
+    }
+}
+
+function setCurrentMode(newMode) {
+    activeButton(newMode);
+    currentMode = newMode;
+  }
