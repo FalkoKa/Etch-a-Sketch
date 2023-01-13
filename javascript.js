e28b67a5116561e48cf6942f6e512ac04a508845
falko <115454465+FalkoKa@users.noreply.github.com>
2023-01-13 12:28:18 +0700

    Finish version 1 without grid size and coloring

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
 const grid = document.querySelector('.grid');
+const btnSize = document.querySelector('#grid-size');
+
+btnSize.addEventListener('click', getGridSize);
 
 function createSquares(size) {
     for (let i = size; i > 0; i--) {
         const square = document.createElement('div');
         square.classList.add('square');
         square.addEventListener('mouseover', changeColor);
+        // square.addEventListener('mousedown', changeColor);
         grid.append(square);
     }
 }
@@ -12,3 +16,13 @@ function createSquares(size) {
 function changeColor () {
     this.classList.add('hov')
 }
+
+function getGridSize() {
+    let gridSize = parseInt(prompt('Choose the grid size'))
+
+    if (gridSize < 1) { gridSize = parseInt(prompt('Choose a grid size bigger than 0'))}; 
+    if (gridSize > 99) { gridSize = parseInt(prompt('Choose a grid size smaller than 100'))}; 
+
+return gridSize;
+}
+
