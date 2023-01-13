c2d99427c2cf8913772d82e71f0a8cdc3d1cf619
falko <115454465+FalkoKa@users.noreply.github.com>
2023-01-13 17:17:29 +0700

    Finished v2

--- javascript.js
+++ javascript.js
@@ -1,9 +1,15 @@
 const grid = document.querySelector('.grid');
 const btnSize = document.querySelector('#grid-size');
 const btnClear = document.querySelector('#clear');
+let gridSize = 16;
 
 btnSize.addEventListener('click', getGridSize);
 btnClear.addEventListener('click', clearGrid);
+
+
+createSquares(gridSize);
+
+
 
 function createSquares(size) {
     for (let i = size * size; i > 0; i--) {
@@ -23,16 +29,21 @@ function changeColor () {
 }
 
 function getGridSize() {
-    let gridSize = parseInt(prompt('Choose the grid size'))
+    clearGridNoNewGrid();
+    gridSize = parseInt(prompt('Choose the grid size'))
 
     if (gridSize < 1) { gridSize = parseInt(prompt('Choose a grid size bigger than 0'))}; 
-    if (gridSize > 99) { gridSize = parseInt(prompt('Choose a grid size smaller than 100'))}; 
-
-return gridSize;
+    if (gridSize > 64) { gridSize = parseInt(prompt('Choose a grid between 1 and 64'))}; 
+    return createSquares(gridSize);
 }
 
 function clearGrid() {
-    // remove all divs
     const squares = document.querySelectorAll('.square');
-    squares.forEach(square => square.remove())
+    squares.forEach(square => square.remove());
+    createSquares(gridSize);
+}
+
+function clearGridNoNewGrid() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.remove());
 }
